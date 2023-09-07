a363bca5c7caf63527c1eaa216527ce7d75a9241
Anders Aarsæther <Anders.aarsaetheren@gmail.com>
2023-09-07 16:21:12 +0200

    Added message for Vilde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.text}> Hei Vilde! </h1>
+    </div>
+  );
 }
