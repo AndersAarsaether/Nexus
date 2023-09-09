fdb1f171f5b1219b5910c626abf26418bd6c4f53
VildeEllingsen <vildeellingsen@outlook.com>
2023-09-09 19:51:25 +0200

    Endret tekst

--- src/app/dates/page.tsx
+++ src/app/dates/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css"
 export default function DateNights(){
   return (
     <div className={styles.container}> 
-      <h1 className={styles.lovetext}>Anders elsker Vilde</h1>
+      <h1 className={styles.lovetext}>Anders elsker Vilde og Gilbert</h1>
     </div>
   );
 }
