096d04b622d95eec747068db452f8ca16bdfb1d2
AndersAarsaether <584975@stud.hvl.no>
2023-09-10 16:07:51 +0200

    Added ID to iterator

--- src/app/dates/page.tsx
+++ src/app/dates/page.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 
-
-export default function DateNights(){
-
+export default function DateNights() {
   const [dates, setDates] = useState<string[]>([]);
 
   const handleAddDate = () => {
-    const inputElement = document.getElementById('input') as HTMLInputElement;
+    const inputElement = document.getElementById("input") as HTMLInputElement;
     const newDate = inputElement.value;
     setDates([...dates, newDate]);
-    inputElement.value = '';
+    inputElement.value = "";
   };
 
   return (
@@ -22,22 +20,21 @@ export default function DateNights(){
         <h2 className={styles.lovetext}>Her kan man legge inn date-ideer!</h2>
       </div>
       <div>
-          <ul>
-            {dates.map(date => 
-              <li>{date}</li>
-            )}
-          </ul>
+        <ul>
+          {dates.map((date) => (
+            <li id={date}>{date}</li>
+          ))}
+        </ul>
       </div>
       <div>
         <form>
-          <input type="text" id="input"/>
-          <button type="button" onClick={handleAddDate}> Legg til </button>
+          <input type="text" id="input" />
+          <button type="button" onClick={handleAddDate}>
+            {" "}
+            Legg til{" "}
+          </button>
         </form>
       </div>
-  </div>
+    </div>
   );
 }
-
-
-
-
